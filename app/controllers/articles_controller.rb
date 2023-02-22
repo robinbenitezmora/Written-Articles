c3cbb49738c5677aa3bca20e6a3cb2332ba50018
robinbenitezmora <robinbenitez1990@gmail.com>
2023-02-22 12:29:49 -0500

    Modify routes and add 'new.html.erb' file for the new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,19 @@ class ArticlesController < ApplicationController
 
   def show
     @lecture = Lecture.find(params[:id])
+  end
+
+  def new
+    @lecture = Lecture.new
+  end
+
+  def create
+    @lecture = Lecture.new(title: "...", body: "...")
+
+    if @lecture.save
+      redirect_to @lecture
+    else
+      render :new, status: :unprocessable_entity      
+    end
+  end
 end
